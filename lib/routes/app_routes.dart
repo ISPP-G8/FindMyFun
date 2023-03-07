061779588eed52d6cf1b61496d2ae53c1437bd68
albgalhue <albgalhue@alum.us.es>
2023-03-07 16:54:15 +0100

    feat: Creacion pantalla de perfil

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,16 +1,13 @@
 import 'package:findmyfun/screens/screens.dart';
 import 'package:flutter/material.dart';
 
+Map<String, Widget Function(BuildContext)> exportRoutes() {
+  Map<String, Widget Function(BuildContext)> routes = {
+    'login': (_) => const LoginView(),
+    'register': (_) => const RegisterScreen(),
+    'access': (_) => const AccessScreen(),
+    'profile': (_) => const ProfileDetailsView(),
+  };
 
-Map<String, Widget Function(BuildContext)> exportRoutes(){
-   Map<String, Widget Function(BuildContext)> routes = {
-
-    'login':(_) => const LoginView(),
-    'register':(_) => const RegisterScreen(),
-    'access':(_) => const AccessScreen(),
-
-   };
-
-   return routes;
+  return routes;
 }
-
---
+++ lib/views/profile/profile_details_view.dart
@@ -0,0 +1,75 @@
+import 'package:findmyfun/themes/colors.dart';
+import 'package:findmyfun/themes/styles.dart';
+import 'package:findmyfun/widgets/submit_button.dart';
+import 'package:flutter/material.dart';
+import 'package:findmyfun/services/page_view_service.dart';
+import 'package:provider/provider.dart';
+
+class ProfileDetailsView extends StatelessWidget {
+  const ProfileDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+
+    final pageViewController =
+        Provider.of<PageViewService>(context).pageController;
+    return Scaffold(
+      backgroundColor: ProjectColors.primary,
+      appBar: AppBar(
+        leading: GestureDetector(
+            onTap: () => pageViewController.animateToPage(0,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut),
+            child: const Icon(
+              Icons.chevron_left,
+              size: 45,
+            )),
+        backgroundColor: ProjectColors.primary,
+        elevation: 0,
+        centerTitle: true,
+        title: Text('MI PERFIL',
+            textAlign: TextAlign.center, style: Styles.appBar),
+      ),
+      body: Center(
+        child: Container(
+          //margin: const EdgeInsets.symmetric(horizontal: 30),
+          height: 850,
+          width: 475,
+          decoration: BoxDecoration(
+            color: const Color(0xff828a92),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.account_circle, // icono de foto de perfil
+                size: 200,
+                color: Color.fromARGB(255, 67, 32, 32),
+              ),
+              SizedBox(height: 20),
+              for (var i = 0; i < 7; i++)
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                ),
+              SizedBox(height: 30),
+              const SubmitButton(text: 'CONTINUAR')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
