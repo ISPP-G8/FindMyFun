6a596bbaa56c5149c6ec1d7196f4decd9358e862
Jesús Martín Vergara <jmartin@youforget.me>
2023-03-07 16:52:30 +0100

    fix: Conflictos resueltos

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,7 +1,4 @@
 export 'package:findmyfun/screens/access_screen.dart';
 export 'package:findmyfun/screens/register_screen.dart';
-<<<<<<< HEAD
-=======
 export 'package:findmyfun/views/access/login_view.dart';
 export 'package:findmyfun/views/preferences/preferences_view.dart';
->>>>>>> origin/Vista-de-preferencias-frontend
