b15152cdb305bbb3bf29f843215754be3fa8d9b4
davsabtro <david.satro@yahoo.es>
2023-03-09 17:24:20 +0100

    feat: Accede  a eventos

--- lib/services/events_service.dart
+++ lib/services/events_service.dart
@@ -7,6 +7,16 @@ import '../models/models.dart';
 
 class EventsService extends ChangeNotifier {
   final String _baseUrl = 'findmyfun-c0acc-default-rtdb.firebaseio.com';
+  
+  List<dynamic> _events = [];
+
+  List<dynamic> get events => _events;
+
+  void set setEvents(List<dynamic> inputEvents) {
+    _events = inputEvents;
+    notifyListeners();
+  }
+
   //POST AND UPDATE EVENT
   Future<void> saveEvent(Event event) async {
     final url = Uri.https(_baseUrl, 'Events/${event.id}.json');
@@ -16,4 +26,28 @@ class EventsService extends ChangeNotifier {
       print('Error creating event: $e');
     }
   }
+
+  //READ EVENT
+  Future<void> getEvents() async {
+    final url = Uri.https(_baseUrl, 'Events.json');
+     try {
+
+      final resp = await http.get(url);
+
+      if (resp.statusCode != 200) {
+        return;
+      }
+
+      Map<String, dynamic> data = jsonDecode(resp.body);
+
+      data.forEach((key, value) {
+        final event = Event.fromRawJson(jsonEncode(value));
+        _events.add(event);
+      });
+
+    } catch (e) {
+      print('Error getting events: $e');
+    }
+  }
+
 }
