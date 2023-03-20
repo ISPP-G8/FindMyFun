0870b1f516bbc73d9b887c67dcf8d8168625a626
Jesús Martín Vergara <jmartin@youforget.me>
2023-03-20 17:44:04 +0100

    feat: Obtención de eventos

--- lib/models/event_point.dart
+++ lib/models/event_point.dart
@@ -6,33 +6,34 @@ import 'package:meta/meta.dart';
 import 'dart:convert';
 
 class EventPoint {
-    EventPoint({
-        required this.name,
-        required this.description,
-        required this.longitude,
-        required this.latitude,
-        required this.address,
-        required this.city,
-        required this.country,
-        required this.image,
-        required this.id,
-    });
+  EventPoint({
+    required this.name,
+    required this.description,
+    required this.longitude,
+    required this.latitude,
+    required this.address,
+    required this.city,
+    required this.country,
+    required this.image,
+    required this.id,
+  });
 
-    String name;
-    String description;
-    double longitude;
-    double latitude;
-    String address;
-    String city;
-    String country;
-    String image;
-    String id;
+  String name;
+  String description;
+  double longitude;
+  double latitude;
+  String address;
+  String city;
+  String country;
+  String image;
+  String id;
 
-    factory EventPoint.fromRawJson(String str) => EventPoint.fromJson(json.decode(str));
+  factory EventPoint.fromRawJson(String str) =>
+      EventPoint.fromJson(json.decode(str));
 
-    String toRawJson() => json.encode(toJson());
+  String toRawJson() => json.encode(toJson());
 
-    factory EventPoint.fromJson(Map<String, dynamic> json) => EventPoint(
+  factory EventPoint.fromJson(Map<String, dynamic> json) => EventPoint(
         name: json["name"],
         description: json["description"],
         longitude: json["longitude"].toDouble(),
@@ -42,9 +43,9 @@ class EventPoint {
         country: json["country"],
         image: json["image"],
         id: json["id"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "name": name,
         "description": description,
         "longitude": longitude,
@@ -54,5 +55,8 @@ class EventPoint {
         "country": country,
         "image": image,
         "id": id,
-    };
+      };
+
+  @override
+  String toString() => 'EventPoint(id: $id)';
 }
--- lib/services/event_points_service.dart
+++ lib/services/event_points_service.dart
@@ -11,6 +11,15 @@ import 'users_service.dart';
 
 class EventPointsService extends ChangeNotifier {
   final String _baseUrl = 'findmyfun-c0acc-default-rtdb.firebaseio.com';
+  List<EventPoint> _eventPoints = [];
+
+  List<EventPoint> get eventPoints => _eventPoints;
+
+  set eventPoints(List<EventPoint> val) {
+    _eventPoints = val;
+
+    notifyListeners();
+  }
 
   //POST AND UPDATE EVENT
   Future<void> saveEvent(EventPoint eventPoint) async {
@@ -22,6 +31,27 @@ class EventPointsService extends ChangeNotifier {
       }
     } catch (e) {
       debugPrint('Error creating event point: $e');
+    }
+  }
+
+  Future<void> getItems() async {
+    final url = Uri.https(_baseUrl, 'EventPoints.json');
+
+    try {
+      final resp = await http.get(url);
+      if (resp.statusCode != 200) return;
+
+      Map<String, dynamic> data = jsonDecode(resp.body);
+
+      List<EventPoint> aux = [];
+
+      data.forEach((key, value) {
+        final eventPoint = EventPoint.fromJson(value);
+        aux.add(eventPoint);
+      });
+      eventPoints = aux;
+    } catch (e) {
+      print('Error al obtener los puntos de eventos: $e');
     }
   }
 }
