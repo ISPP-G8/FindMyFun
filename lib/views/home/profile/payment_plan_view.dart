83740875b63c62feefed14fab1c1a1663ba8814a
santizdr <santizuleta11@gmail.com>
2023-04-10 18:14:44 +0200

    Cambio de estilo en la vista

--- lib/views/home/profile/payment_plan_view.dart
+++ lib/views/home/profile/payment_plan_view.dart
@@ -1,165 +1,109 @@
-// ignore_for_file: avoid_print, use_build_context_synchronously
-
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:findmyfun/models/models.dart' as user;
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../../../helpers/validators.dart';
-import '../../../services/services.dart';
-import '../../../themes/colors.dart';
-import '../../../themes/styles.dart';
-import '../../../ui/custom_snackbars.dart';
-import '../../../widgets/widgets.dart';
-
-class PaymentPlanView extends StatelessWidget {
-  const PaymentPlanView({
-    super.key,
-  });
+class PaymentPlanView extends StatefulWidget {
+  const PaymentPlanView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          resizeToAvoidBottomInset: true,
-          // backgroundColor: ProjectColors.primary,
-          body: SingleChildScrollView(
-              child: Column(children: [
-            const Center(
-                child: Text(
-              'CREAR EVENTO',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            )),
-            const SizedBox(
-              height: 20,
-            ),
-            LoginContainer(
-              child: _FormsColumn(),
-            ),
-          ]))),
-    );
-  }
+  _PaymentPlanViewState createState() => _PaymentPlanViewState();
 }
 
-// ignore: must_be_immutable
-class _FormsColumn extends StatelessWidget {
+class _PaymentPlanViewState extends State<PaymentPlanView> {
   final _formKey = GlobalKey<FormState>();
-  final _name = TextEditingController();
-  final _address = TextEditingController();
-  final _city = TextEditingController();
-  final _country = TextEditingController();
-  final _latitude = TextEditingController();
-  final _longitude = TextEditingController();
-  final _description = TextEditingController();
-  final _image = TextEditingController();
-  final _startDateTime = TextEditingController();
-  final _startTime = TextEditingController();
-  List<Object> _selectedValues = [];
-  // final _tags = const CategoryDropdown(onSelectionChanged: ,);
-
-  _FormsColumn();
+  String _cardNumber = '';
+  String _expirationDate = '';
+  String _cvv = '';
 
   @override
   Widget build(BuildContext context) {
-    String? id = AuthService().currentUser?.uid ?? "";
-    return Form(
-      // autovalidateMode: AutovalidateMode.onUserInteraction,
-      key: _formKey,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 10,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Introducir Credenciales'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 100.0, 16.0, 0.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Introducir credenciales',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 35.0),
+              TextFormField(
+                decoration: InputDecoration(
+                  labelText: 'Número de tarjeta',
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Por favor ingrese el número de la tarjeta';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    _cardNumber = value;
+                  });
+                },
+              ),
+              Row(children: [
+                Expanded(
+                    child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Fecha de expiración',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Por favor ingrese la fecha de expiración';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      _expirationDate = value;
+                    });
+                  },
+                )),
+                SizedBox(width: 12.0),
+                Expanded(
+                    child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'CVV',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Por favor ingrese el CVV';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      _cvv = value;
+                    });
+                  },
+                ))
+              ]),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      // Validación pasada, hacer algo con los datos
+                      print('Número de tarjeta: $_cardNumber');
+                      print('Fecha de expiración: $_expirationDate');
+                      print('CVV: $_cvv');
+                    }
+                  },
+                  child: Text('Procesar Pago'),
+                ),
+              ),
+            ],
           ),
-          Text(
-            'DATOS DE PAGO',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: ProjectColors.primary,
-                fontWeight: FontWeight.bold,
-                fontSize: 35),
-          ),
-          CustomTextForm(
-            hintText: 'Nombre del titular',
-            controller: _name,
-            validator: (value) => Validators.validateNotEmpty(value),
-          ),
-          CustomTextForm(
-            hintText: 'Numero de tarjeta',
-            controller: _address,
-            validator: (value) => Validators.validateNotEmpty(value),
-          ),
-          CustomTextForm(
-            hintText: 'Caducidad',
-            controller: _city,
-            validator: (value) => Validators.validateNotEmpty(value),
-          ),
-          CustomTextForm(
-            hintText: 'CVV',
-            controller: _country,
-            validator: (value) => Validators.validateNotEmpty(value),
-          ),
-          SubmitButton(
-            text: 'CONTINUAR',
-            onTap: () async {
-              if (_formKey.currentState!.validate() &&
-                  _selectedValues.isNotEmpty) {
-                showDialog(
-                  context: context,
-                  builder: (context) => Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        SizedBox(
-                            height: 50,
-                            width: 50,
-                            child: CircularProgressIndicator()),
-                      ]),
-                );
-                /* final eventsService =
-                    Provider.of<EventsService>(context, listen: false);
-                await eventsService.saveEvent(Event(
-                    address: _address.text,
-                    city: _city.text,
-                    country: _country.text,
-                    description: _description.text,
-                    finished: false,
-                    image: _image.text,
-                    name: _name.text,
-                    latitude: double.parse(_latitude.text),
-                    longitude: double.parse(_longitude.text),
-                    startDate: DateTime.parse(
-                        '${_startDateTime.text} ${_startTime.text}'),
-                    tags: await Future.wait(_selectedValues
-                        .map((e) => PreferencesService()
-                            .getPreferenceByName(e.toString()))
-                        .toList()),
-                    users: [id],
-                    messages: [
-                      Messages(
-                          userId: "8AH3CM76DydLFLrAQANT2gTBYlk2",
-                          date: DateTime.now(),
-                          text: "Bienvenido")
-                    ],
-                    id: const Uuid().v1())); */
-                // ignore: use_build_context_synchronously
-                Navigator.pop(context);
-                final pageController =
-                    // ignore: use_build_context_synchronously
-                    Provider.of<PageViewService>(context, listen: false);
-                pageController.mainPageController.jumpToPage(0);
-              } else {
-                CustomSnackbars.showCustomSnackbar(
-                  context,
-                  const Text('Rellene los campos'),
-                );
-              }
-            },
-          )
-        ],
+        ),
       ),
     );
   }
