623d0700c8ac6c1d697e9c20b0535989e8081fd2
Roberto <antsermen@alum.us.es>
2023-04-12 15:50:05 +0200

    upload: Avance de los tests (no funciona)

---
+++ android/app/src/androidTest/java/lib/main/findmyfun/MainActivityTest.java
@@ -0,0 +1,11 @@
+package lib;
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivityTest> rule = new ActivityTestRule<>(MainActivityTest.class, true, false);
+}
