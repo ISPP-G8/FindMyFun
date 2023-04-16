7add22268cedfacbad739342a8322ad22c09d4c7
Álvaro Gómez Nieto <alvgomnie@alum.us.es>
2023-04-16 14:00:52 +0200

    Arreglo de username en la vista de chats

--- lib/views/event/event_chat_view.dart
+++ lib/views/event/event_chat_view.dart
@@ -1,6 +1,7 @@
 import 'package:findmyfun/helpers/validators.dart';
 import 'package:findmyfun/models/event.dart';
 import 'package:findmyfun/models/messages.dart';
+import 'package:findmyfun/models/models.dart';
 import 'package:findmyfun/services/services.dart';
 import 'package:findmyfun/widgets/custom_banner_ad.dart';
 import 'package:findmyfun/widgets/custom_text_form.dart';
@@ -30,7 +31,7 @@ class _ChatScreenState extends State<ChatScreen> {
     final selectedEvent = ModalRoute.of(context)!.settings.arguments as Event;
     messages = selectedEvent.messages;
     final messagesService = Provider.of<MessagesService>(context);
-    final userService = Provider.of<UsersService>(context, listen: false);
+    final userService = UsersService();
     String activeUserId = AuthService().currentUser?.uid ?? "";
     final messageToSend = TextEditingController();
 
@@ -55,6 +56,10 @@ class _ChatScreenState extends State<ChatScreen> {
                 messages.sort((a, b) => a.date.compareTo(b.date));
                 final message = messages[index];
                 final bool isMe = message.userId == activeUserId;
+                Future<String> getUserName() async{
+                  final User user = await userService.getUserWithUid(message.userId);
+                  return user.username;
+                }
                 return Container(
                   margin: EdgeInsets.only(
                     top: 8.0,
@@ -82,13 +87,22 @@ class _ChatScreenState extends State<ChatScreen> {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
-                        message.userId,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 12.0,
+                      FutureBuilder<String>(
+                          future: getUserName().timeout(const Duration(seconds: 1)),
+                          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                            if (snapshot.hasData) {
+                              return Text(
+                                snapshot.data!,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                ),
+                              );
+                            } else {
+                              return const CircularProgressIndicator();
+                            }
+                          },
                         ),
-                      ),
                       const SizedBox(
                           height:
                               8.0), // Agrega un espacio vertical de 8.0 píxeles
