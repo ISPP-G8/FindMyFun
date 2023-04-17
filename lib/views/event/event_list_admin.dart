5e5c8ea168690703a3529344f8fa91a74cb12988
santizdr <santizuleta11@gmail.com>
2023-04-17 18:13:31 +0200

    Previo a administracion de eventos

--- lib/views/event/event_list_admin.dart
+++ lib/views/event/event_list_admin.dart
@@ -74,9 +74,10 @@ class _EventsColumn extends StatelessWidget {
               itemBuilder: (context, index) {
                 final event = events[index];
                 return ListTile(
-                  title: Text(event.name),
-                  subtitle: Text('${event.address} \n${event.city}'),
-                );
+                    title: Text(event.name),
+                    subtitle: Text('${event.address} \n${event.city}'),
+                    onTap: () => Navigator.pushNamed(context, 'eventDetails',
+                        arguments: event));
               },
               separatorBuilder: (context, index) => const Divider(),
             )),
