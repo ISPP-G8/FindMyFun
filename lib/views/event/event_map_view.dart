ffbb5e2ac441dba8c6fd88e1759c852ecae7edf1
José Manuel Martín Luque <josemanuelmartinluque@outlook.com>
2023-05-20 19:54:21 +0200

    refactor: Modifica los toast de las vistas con mapa

--- lib/views/event/event_map_view.dart
+++ lib/views/event/event_map_view.dart
@@ -70,14 +70,7 @@ class _MapScreenState extends State<MapScreen> {
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      Fluttertoast.showToast(
-          msg: "Location services are disabled",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.CENTER,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
+      Fluttertoast.showToast(msg: "Servicios de localización desactivados");
       currentPosition = defaultPosition;
       hasCurrentPositionBeenUpdated = true;
       return null;
@@ -87,14 +80,7 @@ class _MapScreenState extends State<MapScreen> {
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        Fluttertoast.showToast(
-            msg: "Location permissions are denied",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.CENTER,
-            timeInSecForIosWeb: 1,
-            backgroundColor: Colors.red,
-            textColor: Colors.white,
-            fontSize: 16.0);
+        Fluttertoast.showToast(msg: "Permisos de localización rechazados");
         currentPosition = defaultPosition;
         hasCurrentPositionBeenUpdated = true;
         return null;
@@ -103,13 +89,7 @@ class _MapScreenState extends State<MapScreen> {
 
     if (permission == LocationPermission.deniedForever) {
       Fluttertoast.showToast(
-          msg: "Location permissions are permanently denied",
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.CENTER,
-          timeInSecForIosWeb: 1,
-          backgroundColor: Colors.red,
-          textColor: Colors.white,
-          fontSize: 16.0);
+          msg: "Permisos de localización rechazados de forma permanente");
       currentPosition = defaultPosition;
       hasCurrentPositionBeenUpdated = true;
       return null;
